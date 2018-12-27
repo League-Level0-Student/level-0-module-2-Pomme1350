48e1cb7a6a8a0d2666cf19079bfa76f2f9cb350c
Pomme1350 <league@WTS14-2.attlocal.net>
2018-12-26 17:00:52 -0800

    finished magic 8 ball, random intro, unfinished rollercoaster

---
+++ src/rollercoaster.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class rollercoaster {
+public static void main(String[] args) {
+	String height;
+	 height=JOptionPane.showInputDialog(null,"How tall are you in inches?");
+	 int inches=Integer.parseInt(height);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
