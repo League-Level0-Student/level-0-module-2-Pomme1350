df25c1df1bd0a25f0a4918913b763125efd0a16b
Pomme1350 <league@imac-7.attlocal.net>
2019-01-09 16:57:42 -0800

    Icecream drag thing

---
+++ icecream_drag_thing/icecream_drag_thing.pde
@@ -0,0 +1,23 @@
+int x=100;
+int y=100;
+
+
+
+
+
+
+
+
+
+void setup(){
+  size(1000, 1000);
+}
+
+void draw(){
+  fill(#712F2F);
+  ellipse(x, y, 100, 100);
+  if(mousePressed){
+    if(mouseX>x-100&&mouseX<x+100){
+    }
+  }
+}
--- src/rollercoaster.java
+++ src/rollercoaster.java
@@ -5,28 +5,14 @@ public static void main(String[] args) {
 	String height;
 	 height=JOptionPane.showInputDialog(null,"How tall are you in inches?");
 	 int inches=Integer.parseInt(height);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	 if(inches>=48) {
+	 JOptionPane.showMessageDialog(null, "You are tall enough to go onn the roller coaster!!!");
+	 }
+	 
+	 
+	 else {
+		 JOptionPane.showMessageDialog(null, "You cannot go on the roller coaster.");
+	 }
 	
 	
 }
